fa9a4fc7ae3d42dc9c888efcd62ce81be7f761ec
komushi <komushi@gmail.com>
2015-04-08 20:22:14 +0900

    passport

---
+++ server/passport-pivotalcf/lib/strategy.js
@@ -0,0 +1,112 @@
+/**
+ * Module dependencies.
+ */
+// Licensed under the Apache License. See footer for details.
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy, 
+    InternalOAuthError = require('passport-oauth').InternalOAuthError,
+    url = require('url'),
+    util = require('util');
+
+var PROVIDER = 'pivotalcf',
+    SSO_AUTHORIZATION_URL = 'http://localhost:8080/uaa/oauth/authorize',
+    SSO_TOKEN_URL = 'http://localhost:8080/uaa/oauth/token',
+    SSO_PROFILE_URL = 'http://localhost:8080/uaa/userinfo';
+
+function Strategy(options, verify) {
+    options = options || {};
+    options.authorizationURL = options.authorizationURL || SSO_AUTHORIZATION_URL;
+    options.tokenURL = options.tokenURL || SSO_TOKEN_URL;
+    options.profileURL = options.profileURL || SSO_PROFILE_URL;
+    //options.scope = options.scope || ['profile'];
+
+    //Send clientID & clientSecret in 'Authorization' header
+    var auth = 'Basic ' + new Buffer(options.clientID + ':' + options.clientSecret).toString('base64');
+    options.customHeaders = {
+        'Authorization':auth
+    };
+
+    //Store auth in a different variable so we can reset it back.
+    this._origCustomHeader = {
+        'Authorization':auth
+    };
+
+    OAuth2Strategy.call(this, options, verify);
+
+    this.name = PROVIDER;
+
+    //Set AuthMethod as 'Bearer' (used w/ accessToken to perform actual resource actions)
+    this._oauth2.setAuthMethod('Bearer');
+
+    this._oauth2.useAuthorizationHeaderforGET(true);
+
+    //Set default userProfileURI (this is /info endpoint for cloudfoundry.COM)
+    this._userProfileURI = options.profileURL || 'http://localhost:8080/uaa/userinfo';
+    
+}
+
+/**
+ * Inherit from `OAuth2Strategy`.
+ */
+util.inherits(Strategy, OAuth2Strategy);
+
+
+/**
+ * Retrieve user profile from Cloud Foundry.
+ *
+ * This function calls /info endpoint of Cloud Foundry and returns the result
+ * as 'profile'
+ *
+ * @param {String} accessToken
+ * @param {Function} done
+ * @api protected
+ */
+Strategy.prototype.userProfile = function (accessToken, done) {
+    this._oauth2.get(this._userProfileURI, accessToken, function (err, body, res) {
+        if (err) {
+            console.log("err");
+            console.log(err);
+            return done(err);
+        }
+
+        try {
+            console.log("body");
+            console.log(body);
+            console.log("accessToken");
+            console.log(accessToken);
+            done(null, JSON.parse(body));
+        } catch (e) {
+            done(e);
+        }
+    });
+};
+
+Strategy.prototype.reset = function () {
+    this._oauth2._customHeaders = {};
+    this._oauth2._customHeaders['Authorization'] = this._origCustomHeader['Authorization'];
+};
+
+Strategy.prototype.setUserProfileURI = function (userProfileURI) {
+    this._userProfileURI = userProfileURI;
+};
+
+
+
+/**
+ * Override authorizationParams function. In our case, we will check if this._stateParamCallback is
+ * set. If so, we'll call that callback function to set {'state' : 'randomStateVal'}
+ *
+ * @param  {Object} options Hash of options
+ * @return {Object}         {} or {'state' : 'randomStateValFrom__stateParamCallback'}
+ */
+Strategy.prototype.authorizationParams = function(options) {
+    if(this._stateParamCallback) {
+        return {'state': this._stateParamCallback()};
+    }
+  return {};
+};
+
+
+/**
+ * Expose `Strategy`.
+ */
+module.exports = Strategy;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,72 @@
 var application_root = __dirname;
 var express = require("express");
 var request = require('request');
+var session = require('express-session');
 var path = require("path");
 var app = express();
+
+var passport = require('passport');
+var CloudFoundryStrategy = require("./passport-pivotalcf").Strategy;
+
+//Set Cloud Foundry app's clientID
+var CF_CLIENT_ID = 'my_web_app';
+
+//Set Cloud Foundry app's clientSecret
+var CF_CLIENT_SECRET = 'password';
+
+// Note: You should have a app.get(..) for this callback to receive callback from Cloud Foundry
+// For example: If your callback url is: https://myKoolapp.cloudfoundry.com/auth/cloudfoundry/callback
+// then, you should have a HTTP GET endpoint like: app.get('/auth/cloudfoundry/callback', callback))
+//
+var CF_CALLBACK_URL = '/auth/cloudfoundry/callback';
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete CloudFoundry profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+
+
+
+// Use the CloudFoundryStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and CloudFoundry
+//   profile), and invoke a callback with a user object.
+var cfStrategy = new CloudFoundryStrategy({
+    authorizationURL : 'http://localhost:8080/uaa/oauth/authorize',
+    tokenURL : 'http://localhost:8080/uaa/oauth/token',
+    clientID: CF_CLIENT_ID,
+    clientSecret: CF_CLIENT_SECRET,
+    callbackURL: CF_CALLBACK_URL,
+    grant_type: 'authorization_code'
+}, function(accessToken, refreshToken, profile, done) {
+
+    console.log("abc");
+
+    // asynchronous verification, for effect...
+    process.nextTick(function() {
+
+        // To keep the example simple, the user's CloudFoundry profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the CloudFoundry account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+    });
+});
+
+passport.use("pivotalcf", cfStrategy);
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -17,14 +81,79 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
+function ensureAuthenticated(req, res, next) {
+  if(!req.isAuthenticated()) {
+              req.session.originalUrl = req.originalUrl;
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+}
+
 // Config
 app.use(allowCrossDomain);
 app.use(express.static(path.join(application_root, "../client")));
 
-app.get('/api', function (req, res) {
-   res.send('Ecomm API is running');
-   console.log("Hello World");
+app.use(session({ secret: 'keyboard cat',
+          resave: false,
+          saveUninitialized: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// GET /auth/cloudfoundry
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in CloudFoundry authentication will involve
+//   redirecting the user to angellist.co.  After authorization, CloudFoundry
+//   will redirect the user back to this application at /auth/angellist/callback
+app.get('/auth/cloudfoundry', passport.authenticate('pivotalcf'), function(req, res) {
+    // The request will be redirected to CloudFoundry for authentication, so this
+    // function will not be called.
 });
+
+
+// GET /auth/angellist/callback
+//   PS: This MUST match what you gave as 'callback_url' earlier
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/cloudfoundry/callback', passport.authenticate('pivotalcf', {
+    successRedirect: '/api',
+    failureRedirect: '/login'
+}), function(req, res) {
+  console.log("callback");
+    res.redirect('/api');
+});
+
+app.get('/login', function(req, res) {
+    req.session.destroy();
+    req.logout();
+    cfStrategy.reset(); //reset auth tokens
+
+    res.send('<html><body><a href="/auth/cloudfoundry">Sign in with Pivotal CF</a></body></html>');
+
+    // res.render('login', {
+    //     user: req.user
+    // });
+});
+
+app.get('/logout', function(req, res) {
+    res.redirect('/login');
+});
+
+app.get('/api', ensureAuthenticated, function(req, res) {
+   res.send(req.session);
+   // res.send('REST API is running');
+   console.log("Hello World");
+
+});
+
+// app.get('/api', function (req, res) {
+//    res.send('REST API is running');
+//    console.log("Hello World");
+// });
 
 
 app.get('/getGnaviPrefs', function (req, res) {
@@ -130,4 +259,6 @@ app.get('/getGnaviRestByAreaCat', function (req, res) {
 });
 
 
-app.listen(process.env.PORT || 9000);
+var server = app.listen((process.env.PORT || 9000), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
